7e29ef3dd41e8bebecaf05441eeba5c996060e4e
zharzh0 <viianen.ville@gmail.com>
2024-10-21 20:07:01 +0300

    user registration into database

--- KalastusWebsite/Program.cs
+++ KalastusWebsite/Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using KalastusWebsite.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorComponents();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite("Data Source=app.db"));
 
 var app = builder.Build();
 
