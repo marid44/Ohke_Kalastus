2606a58c0ebee2c49620ede8a5a94db283c61d46
marid44 <marimirjamitukki@outlook.com>
2024-12-02 16:41:49 +0200

    osoitteen lisäys

---
+++ KalastusWebsite/Migrations/20241202141714_AddAddressToEvent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KalastusWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddressToEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Events",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Events");
+        }
+    }
+}
--- KalastusWebsite/Models/Event.cs
+++ KalastusWebsite/Models/Event.cs
@@ -7,5 +7,6 @@ namespace KalastusWebsite.Models
         public DateTime Date { get; set; }
         public string Description { get; set; } = string.Empty;
         public string? CreatedBy { get; set; } = string.Empty;
+        public string Address { get; set; } 
     }
 }
